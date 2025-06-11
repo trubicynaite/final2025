8b074a1f7002b729a52a991474e07783aba00931
trubicynaite <trubicynaite@gmail.com>
2025-06-11 22:18:07 +0300

    added styled footer

--- client/src/components/UI/organisms/Footer.tsx
+++ client/src/components/UI/organisms/Footer.tsx
@@ -1,8 +1,36 @@
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+    height: 80px;
+    background-color: #87085D;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    gap: 20px;
+
+    >a{
+        margin: 0 15px;
+        color: white;
+
+        &:hover {
+        text-decoration: underline;
+        }
+    }
+
+    >p{
+        color: white;
+        }
+`
 const Footer = () => {
     return (
-        <footer>
-
-        </footer>
+        <StyledFooter>
+            <p>&copy; 2025 Final Project. TYPE26. All rights reserved.</p>
+            <a href="/privacy-policy">Privacy Policy</a>
+            <a href="/terms-of-service">Terms of Service</a>
+            <a href="/contact">Contact Us</a>
+        </StyledFooter>
     );
 }
 
--- client/src/components/outlets/MainOutlet.tsx
+++ client/src/components/outlets/MainOutlet.tsx
@@ -1,15 +1,20 @@
 import { Outlet } from "react-router";
+import styled from "styled-components";
 
 import Header from "../UI/organisms/Header";
 import Footer from "../UI/organisms/Footer";
+
+const StyledMain = styled.main`
+    min-height: calc(100vh - 80px - 80px);
+`
 
 const MainOutlet = () => {
     return (
         <>
             <Header />
-            <main>
+            <StyledMain>
                 <Outlet />
-            </main>
+            </StyledMain>
             <Footer />
         </>
     );
